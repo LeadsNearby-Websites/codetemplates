68ca8bcac7106c4de863372c99147eefb78b0ac8
Andrew Gillingham <andrew@leadsnearby.com>
2020-01-27 15:41:14 -0500

    Add sourcemaps

--- gulp-style-tasks.js
+++ gulp-style-tasks.js
@@ -3,17 +3,18 @@ import cleancss from 'gulp-clean-css';
 import postcss from 'gulp-postcss';
 import rename from 'gulp-rename';
 import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
 import bulkSass from 'gulp-sass-bulk-import';
 import autoprefixer from 'gulp-autoprefixer';
 import errorHandler from 'gulp-error-handle';
 
 const autoprefixerOptions = {
-  grid: true
+  grid: true,
 };
 
 const sassOptions = {
   errLogToConsole: true,
-  outputStyle: 'nested'
+  outputStyle: 'nested',
 };
 
 const inputdir = './src/scss/**/*.scss';
@@ -28,73 +29,77 @@ const compileMyCss = () => {
       .src(input)
       // .pipe( setTimeout( () => { return }, 500 ) )
       .pipe(errorHandler(log))
+      .pipe(sourcemaps.init())
       .pipe(bulkSass())
       .pipe(sass(sassOptions))
       .pipe(postcss([require('autoprefixer')]))
+      .pipe(autoprefixer(autoprefixerOptions))
+      .pipe(
+        cleancss({
+          format: {
+            breaks: {
+              afterAtRule: true,
+              afterBlockBegins: true,
+              afterBlockEnds: true,
+              afterComment: true,
+              afterProperty: true,
+              afterRuleBegins: true,
+              afterRuleEnds: true,
+              beforeBlockEnds: true,
+              betweenSelectors: true,
+            },
+            indentBy: 2,
+            indentWith: 'space',
+            spaces: {
+              aroundSelectorRelation: true,
+              beforeBlockBegins: true,
+              beforeValue: false,
+            },
+            wrapAt: false,
+          },
+          level: 2,
+        })
+      )
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest(output))
   );
 };
 
-const cleanMyCss = () => {
-  return gulp
-    .src('./dist/css/style.css')
-    .pipe(errorHandler(log))
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(
-      cleancss({
-        format: {
-          breaks: {
-            afterAtRule: true,
-            afterBlockBegins: true,
-            afterBlockEnds: true,
-            afterComment: true,
-            afterProperty: true,
-            afterRuleBegins: true,
-            afterRuleEnds: true,
-            beforeBlockEnds: true,
-            betweenSelectors: true
-          },
-          indentBy: 2,
-          indentWith: 'space',
-          spaces: {
-            aroundSelectorRelation: true,
-            beforeBlockBegins: true,
-            beforeValue: false
-          },
-          wrapAt: false
-        },
-        level: 2
-      })
-    )
-    .pipe(gulp.dest('./'));
-};
-
 const minifyMyCss = () => {
   return gulp
-    .src('./dist/css/style.css')
+    .src(input)
     .pipe(errorHandler(log))
+    .pipe(sourcemaps.init())
+    .pipe(bulkSass())
+    .pipe(sass(sassOptions))
+    .pipe(postcss([require('autoprefixer')]))
+    .pipe(autoprefixer(autoprefixerOptions))
     .pipe(
       cleancss({
         level: {
           1: {
             all: true,
-            specialComments: 0
-          }
-        }
+            specialComments: 0,
+          },
+        },
       })
     )
     .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/css'));
 };
 
 const watchCss = () => {
-  const watcher = gulp.watch([input, inputdir], gulp.series(compileMyCss, cleanMyCss, minifyMyCss));
+  const watcher = gulp.watch(
+    [input, inputdir],
+    gulp.series(compileMyCss, minifyMyCss)
+  );
   watcher.on('change', function(file) {
     console.log('File ' + file + ' was changed, running tasks...');
   });
 };
 
-const buildCss = gulp.series(compileMyCss, cleanMyCss, minifyMyCss);
+const buildCss = gulp.series(compileMyCss, minifyMyCss);
 const buildWatch = gulp.series(watchCss);
 
 export { buildCss, buildWatch };
