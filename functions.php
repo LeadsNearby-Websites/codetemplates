04b88b496b575a747e755816444f6e4d34001f6d
pipkinbrand <pipkinlandon@gmail.com>
2021-04-13 11:54:37 -0400

    Expanded dist folder to include custom JS file

--- functions.php
+++ functions.php
@@ -15,5 +15,6 @@ if (file_exists(get_stylesheet_directory() . '/inc/custom-header.php')) {include
 if (file_exists(get_stylesheet_directory() . '/inc/custom-sidebar.php')) {include_once get_stylesheet_directory() . '/inc/custom-sidebar.php';}
 if (file_exists(get_stylesheet_directory() . '/inc/custom-footer.php')) {include_once get_stylesheet_directory() . '/inc/custom-footer.php';}
 if (file_exists(get_stylesheet_directory() . '/inc/shortcodes.php')) {include_once get_stylesheet_directory() . '/inc/shortcodes.php';}
+if (file_exists(get_stylesheet_directory() . '/dist/js/custom.js')) {include_once get_stylesheet_directory() . '/dist/js/custom.js';}
 
 /* All other misc code that does not fall into one of the included files can be added below */
