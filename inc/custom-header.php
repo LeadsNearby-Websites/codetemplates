e9a9b5cdc71adee531270b1ced0d4c4e761c58cf
Tim-M-Matthews <tim@leadsnearby.com>
2021-07-28 16:44:39 -0400

    homepage style updates

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -1,3 +1,16 @@
 <?php
 
 /* Custom Header Code */
+
+function output_custom_header() {
+  ob_start();?>
+<div class="ctHeader">
+  <a class="ctHeader__text source-font" href="<?php echo site_url() ?>">LNB Code Templates</a>
+  <a is="hypercore-button" class="ctHeader__btn hypercore-button lnbArrowHover" href="https://github.com/LeadsNearby/code-templates">
+    Go To Repo
+    <fa-icon class="lnbArrowHover__arrow" icon="arrow-right"></fa-icon>
+  </a>
+</div>
+<?php echo ob_get_clean();
+}
+add_action('hypercore_custom_header', 'output_custom_header');
