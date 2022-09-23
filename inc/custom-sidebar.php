c9d738781ff5195010c92c1583a47cd041afd9a9
jgleadsnearby <joe@leadsnearby.com>
2022-09-23 06:36:44 -0400

    updates

--- inc/custom-sidebar.php
+++ inc/custom-sidebar.php
@@ -1,3 +1,27 @@
 <?php
 
-/* Sidebar code with comments */
+function custom_sidebar_slogan() {
+
+ob_start(); ?>
+<section class="lnbSidebarSlogan-backgroundImg hyper-lazyload-bg" data-bg-image="<?php echo content_url('/uploads/JMS-Sidebar-Background.jpeg'); ?>">
+    <section class="lnbSidebarSlogan">
+        <div class="lnbSidebarSlogan__container">
+            <div class="lnbSidebarSlogan__container-slogan">
+                <img class="lnbSidebarSlogan__container-slogan--svg" src="<?php echo content_url('/uploads/lnb-logo.jpeg');?>">
+                <img class="lnbSidebarSlogan__container-slogan--img" src="<?php echo content_url('/uploads/img-indy-car-racing-gray.jpg');?>">
+            </div>
+            <div class="lnbSidebarSlogan__container-links">
+                <ul>
+                    <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 1</a></li>
+                    <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 2</a></li>
+                    <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 3</a></li>
+                    <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</section>
+<?php echo ob_get_clean();
+
+};
+add_filter( 'hypercore_sidebar_html', 'custom_sidebar_slogan' );
--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -2305,3 +2305,55 @@ function output_title_bar() {
     <?php return ob_get_clean();
 }
 add_shortcode('title_bar', 'output_title_bar');
+
+function output_sidebar_slogan() {
+
+  ob_start(); ?>
+  <section class="lnbSidebarSlogan-backgroundImg hyper-lazyload-bg" data-bg-image="<?php echo content_url('/uploads/JMS-Sidebar-Background.jpeg'); ?>">
+      <section class="lnbSidebarSlogan">
+          <div class="lnbSidebarSlogan__container">
+              <div class="lnbSidebarSlogan__container-slogan">
+                  <img class="lnbSidebarSlogan__container-slogan--svg" src="<?php echo content_url('/uploads/lnb-logo.jpeg');?>">
+                  <img class="lnbSidebarSlogan__container-slogan--img" src="<?php echo content_url('/uploads/img-indy-car-racing-gray.jpg');?>">
+              </div>
+              <div class="lnbSidebarSlogan__container-links">
+                  <ul>
+                      <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 1</a></li>
+                      <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 2</a></li>
+                      <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 3</a></li>
+                      <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 4</a></li>
+                  </ul>
+              </div>
+          </div>
+      </section>
+  </section>
+  <?php echo ob_get_clean();
+  
+  };
+  add_shortcode( 'hypercore-sidebar-slogan', 'output_sidebar_slogan' );
+
+  function output_sidebar_links() {
+
+    ob_start(); ?>
+    <aside class="sidebar">
+      <div class="lnbSidebarLinks">
+        <div class="lnbSidebarLinks__img">
+          <?php echo hypercore_get_lazyload_image( '502' ); ?>
+        </div>
+        <div class="lnbSidebarLinks__inner">
+          <div class="lnbSidebarLinks__cta">Text or elements can go here</div>
+          <div class="lnbSidebarLinks__menu">
+            <ul>
+                <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 1</a></li>
+                <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 2</a></li>
+                <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 3</a></li>
+                <li class="lnbArrowHover"><fa-icon class="lnbArrowHover__arrow" icon="arrow-right" family="solid"></fa-icon><a href="<?php echo site_url('/'); ?>">Service Link 4</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </aside>
+    <?php echo ob_get_clean();
+    
+    };
+    add_shortcode( 'hypercore-sidebar-links', 'output_sidebar_links' );
